396b255f4f867ec5925abb3249576ce660c2139d
FabMolina05 <fabian.molina5005@gmail.com>
2025-11-25 22:53:31 -0600

    primera version de sidenav

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'src/Web/components/Header.php'; ?> 
+<?php include 'src/Web/components/Sidebar.php'; ?>
+
+<div class="main-content">
+     <h1>Contenido Principal</h1>
+    <p>Aquí va el contenido de tu aplicación.</p>
+</div>
+
+<?php include 'src/Web/components/Footer.php'; ?>
+    
+
+
+
